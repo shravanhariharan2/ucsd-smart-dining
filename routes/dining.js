197ba8fe4a628b3ddaf8f7c39689f3d06d037090
Shravan Hariharan <shravanhariharan2@gmail.com>
2019-10-26 21:49:12 -0700

    added reading from firebase

--- routes/dining.js
+++ routes/dining.js
@@ -1,6 +1,29 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => res.render('dining'));
+router.get('/', (req, res) => {
+
+    let sixtyFour = req.app.get('64 Degrees');
+    let pines = req.app.get('Pines');
+    let cafeV = req.app.get('Cafe Ventanas');
+    let roots = req.app.get('Roots');
+    let wft = req.app.get('Warren Food Trucks');
+    let clubMed = req.app.get('Club Med');
+    let ovt = req.app.get('OVT');
+    let foodworx = req.app.get('Foodworx');
+    let goodys = req.app.get('Goody\'s');
+
+    res.render('dining', { 
+        sixtyFour : sixtyFour,
+        pines : pines,
+        cafeV : cafeV,
+        roots : roots,
+        ovt : ovt,
+        wft : wft,
+        clubMed : clubMed,
+        foodworx : foodworx,
+        goodys : goodys
+    });
+});
 
 module.exports = router;
