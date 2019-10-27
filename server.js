bf6f667e53171aaf722b067da0be545c3d66bb33
Shravan Hariharan <shravanhariharan2@gmail.com>
2019-10-26 18:53:34 -0700

    connected filesystem

--- server.js
+++ server.js
@@ -2,27 +2,29 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const admin = require('firebase-admin');
 const serviceAccount = require("./config/ServiceAccountKey.json");
+const expressLayout = require('express-ejs-layouts');
 
 const app = express();
+
+// EJS
+app.use(express.static("./views/static"));
+app.set('view engine', 'ejs');
 
 //Database initialization
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: 'https://ucsd-smart-dining.firebaseio.com'
 })
-
 const db = admin.firestore();
-const sampleData = {
-    quote: "Hello world!",
-    author: "Shravan"
-}
-db.collection('sampleData').doc('inspiration').set(sampleData)
-  .then(() => console.log("Data added"))
-  .catch((err) => console.log(err));
 
 // BodyParser Middleware
 app.use(bodyParser.json());
 
+// Routes
+app.use('/', require('./routes/index'));
+app.use('/dining', require('./routes/dining'));
+app.use('/calorie', require('./routes/calorie'));
+app.use('/about', require('./routes/about'));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
